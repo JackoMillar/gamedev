429f73043c43ed8ec29ad089ed0b1423e4b9b4d8
Jack Millar <99406899+JackoMillar@users.noreply.github.com>
2024-09-20 16:29:23 +0100

    Displaying mutliple different types of space invaders

--- 2_invaders/main.cpp
+++ 2_invaders/main.cpp
@@ -1,5 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+//main.cpp
+#include "ship.h"
+//...
+std::vector<Ship*> ships;
 using namespace std;
 using namespace sf;
 
@@ -15,10 +19,51 @@ void Load() {
     }
     invader.setTexture(spritesheet);
     invader.setTextureRect(IntRect(Vector2i(0, 0), Vector2i(32, 32)));
+
+    // Add first invader
+    Invader* inv1 = new Invader(sf::IntRect(Vector2i(0, 0), Vector2i(32, 32)), { 100,100 });
+    ships.push_back(inv1);
+
+    // Add second invader (at different texture and position)
+    Invader* inv2 = new Invader(sf::IntRect(Vector2i(32, 0), Vector2i(32, 32)), { 200,500 });
+    ships.push_back(inv2);
+
+    // Add third invader (different position)
+    Invader* inv3 = new Invader(sf::IntRect(Vector2i(64, 0), Vector2i(32, 32)), { 300,100 });
+    ships.push_back(inv3);
+
+    // Add third invader (different position)
+    Invader* inv4 = new Invader(sf::IntRect(Vector2i(96, 0), Vector2i(32, 32)), { 400,200 });
+    ships.push_back(inv4);
+
+
+}
+
+void Update(RenderWindow& window) {
+
+    // Reset clock, recalculate deltatime
+    static Clock clock;
+    float dt = clock.restart().asSeconds();
+    // check and consume events
+    Event event;
+    while (window.pollEvent(event)) {
+        if (event.type == Event::Closed) {
+            window.close();
+            return;
+        }
+    }
+
+    for (auto& s : ships) {
+        s->Update(dt);
+    };
 }
 
 void Render(RenderWindow& window) {
     window.draw(invader);
+
+    for (const auto s : ships) {
+        window.draw(*s);
+    }
 }
 
 int main() {
@@ -26,6 +71,7 @@ int main() {
     Load();
     while (window.isOpen()) {
         window.clear();
+        Update(window);
         Render(window);
         window.display();
     }
