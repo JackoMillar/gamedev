3d0bbdf9f2b433afef20290b780cbba53bd2d037
Jack Millar <99406899+JackoMillar@users.noreply.github.com>
2024-09-20 14:45:04 +0100

    showing a single space invader

--- 2_invaders/main.cpp
+++ 2_invaders/main.cpp
@@ -1,19 +1,32 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
+using namespace sf;
+
+const int gameWidth = 800;
+const int gameHeight = 600;
+
+Texture spritesheet;
+Sprite invader;
+
+void Load() {
+    if (!spritesheet.loadFromFile("res/sprites/invaders_sheet.png")) {
+        cerr << "Failed to load spritesheet!" << std::endl;
+    }
+    invader.setTexture(spritesheet);
+    invader.setTextureRect(IntRect(Vector2i(0, 0), Vector2i(32, 32)));
+}
+
+void Render(RenderWindow& window) {
+    window.draw(invader);
+}
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Red);
-
+    RenderWindow window(VideoMode(gameWidth, gameHeight), "SPACE INVADERS");
+    Load();
     while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
         window.clear();
-        window.draw(shape);
+        Render(window);
         window.display();
     }
     return 0;
